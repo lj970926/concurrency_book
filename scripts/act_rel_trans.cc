e31d6806ef09b27ea2e8c6ee2cf80590c979c0c3
lj970926 <1783973490@qq.com>
2026-05-03 11:43:29 +0800

    fix: use release sequence in transitivity example

--- scripts/act_rel_trans.cc
+++ scripts/act_rel_trans.cc
@@ -7,23 +7,26 @@
 #include <cassert>
 
 
-std::atomic<bool> sync1 = false, sync2 = false;
+std::atomic<int> sync = 0;
 std::atomic<int> data[5] = {-1, -1, -1, -1, -1};
 
 void thread_1() {
     for (int i = 0; i < 5; ++i) {
         data[i].store(i, std::memory_order_relaxed);
     }
-    sync1.store(true, std::memory_order_release);
+    sync.store(1, std::memory_order_release);
 }
 
 void thread_2() {
-    while (!sync1.load(std::memory_order_acquire)) ;
-    sync2.store(true, std::memory_order_release);
+    int expected = 1;
+    while (!sync.compare_exchange_strong(expected, 2, std::memory_order_acq_rel)) {
+        expected = 1;
+    }
 }
 
 void thread_3() {
-    while (!sync2.load(std::memory_order_acquire)) ;
+    while (sync.load(std::memory_order_acquire) < 2
+) ;
     for (int i = 0; i < 5; ++i) {
         assert(data[i] == i);
     }
